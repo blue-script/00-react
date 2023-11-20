537864c7b69a756c9b3b69c806fc7549bd2fcd2e
demianov-an <Alexm92@inbox.ru>
2023-11-20 17:28:37 +0600

    lesson 6 and 7

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import Accordion from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
 import OnOff from './components/OnOff/OnOff';
+import UncontrolledAccordion from './components/UncontrolledAccordion/UncontrolledAccordion';
+import {UncontrolledRating} from './components/UncontrolledRating/UncontrolledRating';
 
 // function declaration
 function App() {
@@ -10,22 +12,26 @@ function App() {
   console.log("App rendering")
   // обязана вернуть JSX
   return (
-      <div>
-        <PageTitle title={"This is APP component"}/>
+      <div className='App'>
+        <UncontrolledAccordion titleValue={"menu"}/>
+        <UncontrolledAccordion titleValue={"menu2"}/>
 
-        <Accordion titleValue={"menu"} collapsed={true}/>
-        <Accordion titleValue={"menu2"} collapsed={false}/>
+        <UncontrolledRating/>
 
-        Article 2
-        <Rating value={0}/>
-        <Rating value={1}/>
-        <Rating value={2}/>
-        <Rating value={3}/>
-        <Rating value={4}/>
-        <Rating value={5}/>
+        {/*<OnOff/>*/}
 
-        <OnOff isActive={true}/>
-        <OnOff isActive={false}/>
+        {/*<PageTitle title={"This is APP component"}/>*/}
+
+        {/*<Accordion titleValue={"menu"} collapsed={true}/>*/}
+        {/*<Accordion titleValue={"menu2"} collapsed={false}/>*/}
+
+        {/*Article 2*/}
+        {/*<Rating value={0}/>*/}
+        {/*<Rating value={1}/>*/}
+        {/*<Rating value={2}/>*/}
+        {/*<Rating value={3}/>*/}
+        {/*<Rating value={4}/>*/}
+        {/*<Rating value={5}/>*/}
       </div>
   );
 }
--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -1,16 +1,44 @@
-import React from 'react';
-import Button from './Button';
+import React, {useState} from 'react';
 
-type OnOffPropsType = {
-  isActive: boolean
+type PropsType = {
+  // on: boolean
 }
 
-const OnOff: React.FC<OnOffPropsType> = (props) => {
+const OnOff: React.FC<PropsType> = (props) => {
+  let [on, setOn] = useState(false)
+
+  const onStyle = {
+    width: '30px',
+    height: '20px',
+    border: '1px solid black',
+    display: 'inline-block',
+    padding: '2px',
+    backgroundColor: on ? 'green' : 'white',
+  }
+  const offStyle = {
+    width: '30px',
+    height: '20px',
+    border: '1px solid black',
+    display: 'inline-block',
+    marginLeft: '2px',
+    padding: '2px',
+    backgroundColor: on ? 'white' : 'red',
+  }
+  const indicatorStyle = {
+    border: '1px solid black',
+    width: '10px',
+    height: '10px',
+    borderRadius: '5px',
+    display: 'inline-block',
+    marginLeft: '5px',
+    backgroundColor: on ? 'green' : 'red',
+  }
+
   return (
-    <div className={'onOff'}>
-      <Button className={`${props.isActive && "bgGreen"}`} title={'ON'}/>
-      <Button className={`${!props.isActive && "bgRed"}`} title={'OFF'}/>
-      <div className={`circle ${props.isActive ? 'bgGreen' : 'bgRed'}`}></div>
+    <div>
+      <div style={onStyle} onClick={()=>{setOn(true)}}>On</div>
+      <div style={offStyle} onClick={()=>{setOn(false)}}>Off</div>
+      <div style={indicatorStyle}></div>
     </div>
   );
 };
