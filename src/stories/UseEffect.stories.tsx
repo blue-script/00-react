7c401b4b27d82d09616172f0008cf88e9f2404df
demianov-an <alexm92@inbox.ru>
2024-01-25 14:45:17 +0600

    25 - useEffect, cleanup (сброс, зачистка)

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import {clearInterval} from "node:timers";
 
 export default {
   title: 'UseEffect demo'
@@ -9,69 +10,102 @@ export const SimpleExample = () => {
   const [counter, setCounter] = useState<number>(1)
   console.log('SimpleExample')
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log('useEffect every render')
     document.title = counter.toString()
   })
-  useEffect(()=>{
+  useEffect(() => {
     console.log('useEffect only first render (componentDidMount)')
     document.title = counter.toString()
   }, [])
-  useEffect(()=>{
+  useEffect(() => {
     console.log('useEffect first render and every counter change')
     document.title = counter.toString()
   }, [counter])
 
   return <>
     Hello, {counter} {fake}
-    <button onClick={()=>setFake(fake+1)}>fake+</button>
-    <button onClick={()=>setCounter(counter+1)}>counter+</button>
+    <button onClick={() => setFake(fake + 1)}>fake+</button>
+    <button onClick={() => setCounter(counter + 1)}>counter+</button>
   </>
 }
 
-export const SetTimoutExample = () => {
-  const [fake, setFake] = useState(1)
-  const [counter, setCounter] = useState<number>(1)
-  console.log('SetTimoutExample')
-
-  // useEffect(()=>{
-  //   console.log('setTimeout')
-  //   setTimeout(()=>{
-  //     document.title = counter.toString()
-  //   }, 1000)
-  // }, [counter])
-
-  useEffect(()=>{
-    console.log(counter) // будет всегда 1 из за замыкания
-    setInterval(()=>{
-      setCounter((state)=>state+1)
-    }, 1000)
-  }, [])
-
-  return <>
-    Hello, counter: {counter} - fake: {fake}
-    {/*<button onClick={()=>setFake(fake+1)}>fake+</button>*/}
-    {/*<button onClick={()=>setCounter(counter+1)}>counter+</button>*/}
-  </>
-}
-
+// export const SetIntervalExample = () => {
+//   const [fake, setFake] = useState(1)
+//   const [counter, setCounter] = useState<number>(1)
+//   console.log('SetTimoutExample')
+//
+//   useEffect(() => {
+//     console.log(counter) // будет всегда 1 из за замыкания
+//     const intervalId = setInterval(() => {
+//       setCounter((state) => state + 1)
+//     }, 1000)
+//     return () => {
+//       clearInterval(intervalId)
+//     }
+//   }, [])
+//
+//   return <>
+//     Hello, counter: {counter} - fake: {fake}
+//   </>
+// }
 
 export const HomeworkClockExample = () => {
   const [date, setDate] = useState(new Date())
   const h = date.getHours()
   const m = date.getMinutes()
   const s = date.getSeconds()
-  const time = `${h < 10 ? '0'+h: h}:${m< 10 ? '0'+m : m}:${s < 10 ? '0'+s: s}`
+  const time = `${h < 10 ? '0' + h : h}:${m < 10 ? '0' + m : m}:${s < 10 ? '0' + s : s}`
   console.log('HomeworkClockExample')
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log('useEffect')
-    setInterval(()=>{
-      setDate((state)=> new Date())
+    setInterval(() => {
+      setDate((state) => new Date())
     }, 1000)
   }, [])
 
   return <>
     Time: {time}
+  </>
+}
+
+export const ResetEffectExample = () => {
+  const [text, setText] = useState<string>('')
+  console.log('render component with: ' + text)
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      console.log(e.key)
+      setText( text + e.key)
+    }
+    window.addEventListener('keypress', handler)
+    return ()=>{
+      window.removeEventListener('keypress', handler)
+    }
+  }, [text])
+
+  return <>
+    Typed: {text}
+  </>
+}
+
+export const SetTimeoutExample = () => {
+  const [text, setText] = useState<string>('')
+  console.log('render component with: ' + text)
+
+  useEffect(() => {
+    const timeoutId = setTimeout(()=>{
+      console.log('timeout expired')
+      setText('3 seconds passed')
+    }, 3000)
+    return ()=>{
+      console.log('clear')
+      clearTimeout(timeoutId)
+    }
+  }, [text])
+
+  return <>
+    text: {text}
   </>
 }
